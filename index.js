b26243752ac28bb150f5e50e50b1617a86db05b6
hiimbex <hiimbex@github.com>
2017-07-01 11:26:35 -0700

    refactored index.js and added tests

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 module.exports = robot => {
-  robot.on('pull_request.opened', async context => {
-    if (!context.payload.repository.owner || !context.payload.pull_request.user.id || !context.payload.repository.name) {
-        issue = (await context.github.pull_request.get(context.issue())).data;
-    }
-
+  robot.on('pull_request.opened', receive);
+  async function receive(context) {
     const user_login = context.payload.pull_request.user.login;
     const repo_owner_id = context.payload.repository.owner.login;
     const repo_name = context.payload.repository.name;
-    
-    async function getWelcomeFile() {
-        const options = context.repo({path: '.github/new-pr-welcome.md'});
-        const res = await context.github.repos.getContent(options);
-        return new Buffer(res.data.content, 'base64').toString();
-    }
-    const template = getWelcomeFile();
+
+    const options = context.repo({path: '.github/new-pr-welcome.md'});
+    const res = await context.github.repos.getContent(options);
+    const template = Buffer.from(res.data.content, 'base64').toString();
 
     const github = await robot.auth(context.payload.installation.id);
     const response = await github.issues.getForRepo(context.repo({
@@ -24,19 +18,19 @@ module.exports = robot => {
     }));
 
     var count_pr = 0;
-    //check for issues that are also PRs
+    // Check for issues that are also PRs
     for (i = 0; i < response.data.length; i++) {
         if ((response.data[i]).pull_request) {
             count_pr += 1;
         }
-        //exit loop if more than one PR
+        // Exit loop if more than one PR
         if (count_pr > 1) {
             break;
         }
     }
-    //check length of response to make sure its only one pr
+
+    // Check length of response to make sure its only one pr
     if (count_pr === 1) {
         context.github.issues.createComment(context.issue({body: template}));
     }
-  });
-};
+}};
---
+++ test/index.js
@@ -0,0 +1,105 @@
+const expect = require('expect');
+const {createRobot} = require('probot');
+const plugin = require('..');
+const succeedEvent = require('./events/succeedEvent');
+const failEvent = require('./events/failEvent');
+const succIssueRes = require('./events/succIssueRes');
+const failIssueRes = require('./events/failIssueRes');
+
+describe('new-pr-welcome succeed', () => {
+    let robot;
+    let github;
+
+    beforeEach(() => {
+        robot = createRobot();
+        plugin(robot);
+
+        github = {
+            repos: {
+                getContent: expect.createSpy().andReturn(Promise.resolve({
+                    data: {
+                        content: Buffer.from(`Hello World!`).toString('base64')
+                    }
+                }))
+            },
+            issues: {
+                getForRepo: expect.createSpy().andReturn(Promise.resolve(
+                    succIssueRes
+                )),
+                createComment: expect.createSpy()
+            }
+        };
+
+        robot.auth = () => Promise.resolve(github);
+    });
+
+    it('posts a comment because it is a user\'s first PR', async () => {
+        await robot.receive(succeedEvent);
+
+        expect(github.repos.getContent).toHaveBeenCalledWith({
+            owner: 'hiimbex',
+            repo: 'testing-things',
+            path: '.github/new-pr-welcome.md'
+        });
+
+        expect(github.issues.getForRepo).toHaveBeenCalledWith({
+            owner: 'hiimbex',
+            repo: 'testing-things',
+            state: 'all',
+            creator: 'hiimbex-testing'
+        });
+
+        expect(github.issues.createComment).toHaveBeenCalledWith({
+            owner: 'hiimbex',
+            repo: 'testing-things',
+            number: 5,
+            body: 'Hello World!'
+        });
+    });
+});
+
+describe('new-pr-welcome fail', () => {
+    let robot;
+    let github;
+
+    beforeEach(() => {
+        robot = createRobot();
+        plugin(robot);
+
+        github = {
+            repos: {
+                getContent: expect.createSpy().andReturn(Promise.resolve({
+                    data: {
+                        content: Buffer.from(`Hello World!`).toString('base64')
+                    }
+                }))
+            },
+            issues: {
+                getForRepo: expect.createSpy().andReturn(Promise.resolve(
+                    failIssueRes
+                )),
+                createComment: expect.createSpy()
+            }
+        };
+
+        robot.auth = () => Promise.resolve(github);
+    });
+    it('does not post a comment because it is not the user\'s first PR', async () => {
+        await robot.receive(failEvent);
+
+        expect(github.repos.getContent).toHaveBeenCalledWith({
+            owner: 'hiimbex',
+            repo: 'testing-things',
+            path: '.github/new-pr-welcome.md'
+        });
+
+        expect(github.issues.getForRepo).toHaveBeenCalledWith({
+            owner: 'hiimbex',
+            repo: 'testing-things',
+            state: 'all',
+            creator: 'hiimbex'
+        });
+
+        expect(github.issues.createComment).toNotHaveBeenCalled();
+    });
+});
