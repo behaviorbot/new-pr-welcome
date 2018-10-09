cc930169601548ce2a440b9701cefd1808ca8b73
John Sloan <jwsloan@gmail.com>
2018-10-09 07:35:09 -0500

    chore: Update dependencies
    After updating probot, npm audit said there were many vulnerabilities.
    Those were fixed by running `npm audit fix --force`. That lead to an
    update of xo, which required style updates. This commit handles all of
    that.
    
    https://github.com/behaviorbot/welcome/issues/15 #hacktoberfest

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-module.exports = robot => {
-    robot.on('pull_request.opened', receive);
+module.exports = app => {
+    app.on('pull_request.opened', receive);
     async function receive(context) {
-        // Get all issues for repo with user as creator
+    // Get all issues for repo with user as creator
         const response = await context.github.issues.getForRepo(context.repo({
             state: 'all',
             creator: context.payload.pull_request.user.login
@@ -14,9 +14,9 @@ module.exports = robot => {
                 if (config.newPRWelcomeComment) {
                     context.github.issues.createComment(context.issue({body: config.newPRWelcomeComment}));
                 }
-            } catch (err) {
-                if (err.code !== 404) {
-                    throw err;
+            } catch (error) {
+                if (error.code !== 404) {
+                    throw error;
                 }
             }
         }
